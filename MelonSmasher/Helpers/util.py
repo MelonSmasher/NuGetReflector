f85cc29d827877fa9848580a4f3e258e6244d023
Alex Markessinis <markea@sage.edu>
2017-05-26 23:17:34 -0400

    abstracted utility methods to seperate file

---
+++ MelonSmasher/Helpers/util.py
@@ -0,0 +1,145 @@
+from lxml import objectify
+from requests import get
+import hashlib
+import base64
+import sys
+
+
+def _pull(url, json=False):
+    """
+    :param url: 
+    :param json: 
+    :return: 
+    """
+    if json:
+        return get(url, headers={'Accept': 'application/json'})
+    else:
+        response = get(url)
+        if response.status_code == 200:
+            xml = response.content
+            try:
+                response.objectified = objectify.fromstring(xml)
+                return response
+            except Exception as e:
+                print(e.message)
+                return False
+        else:
+            return response
+
+
+def pull_package(title, version, url, json=False):
+    """
+    :param title: 
+    :param version: 
+    :param url: 
+    :param json: 
+    :return: 
+    """
+    return _pull(''.join([url, '(Id=\'', title, '\',Version=\'', version, '\')']), json)
+
+
+def pull_packages(url, json=False):
+    """
+    :param url: 
+    :param json: 
+    :return: 
+    """
+    return _pull(url, json)
+
+
+def sha512sum(file_path, block_size=65536):
+    """
+    :param file_path: 
+    :param block_size:
+    :return: 
+    """
+    return base64.encodestring(
+        __hash_byte_str_iter(__file_as_block_iter(open(file_path, 'rb'), block_size), hashlib.sha512(), False)
+    ).replace("\n", '')
+
+
+def sha256sum(file_path, block_size=65536):
+    """
+    :param file_path: 
+    :param block_size: 
+    :return: 
+    """
+    return base64.encodestring(
+        __hash_byte_str_iter(__file_as_block_iter(open(file_path, 'rb'), block_size), hashlib.sha256(), False)
+    ).replace("\n", '')
+
+
+def sha1sum(file_path, block_size=65536):
+    """
+    :param file_path: 
+    :param block_size: 
+    :return: 
+    """
+    return base64.encodestring(
+        __hash_byte_str_iter(__file_as_block_iter(open(file_path, 'rb'), block_size), hashlib.sha1, False)
+    ).replace("\n", '')
+
+
+def hashes_match(hash_1, hash_2):
+    """
+    :param hash_1: 
+    :param hash_2: 
+    :return: 
+    """
+    if str(hash_1) == str(hash_2):
+        print(' Pass!')
+        return True
+    else:
+        print(' Fail!')
+        print('Hash 1: ' + hash_1)
+        print('Hash 2: ' + hash_2)
+        return False
+
+
+def verify_hash(file_path, target_hash, message='Verifying package hash...', hash_method='sha512'):
+    """
+    :param file_path: 
+    :param target_hash: 
+    :param message: 
+    :param hash_method: 
+    :return: 
+    """
+    sys.stdout.write(message)
+    sys.stdout.flush()
+    hash_method.lower()
+
+    if hash_method == 'sha512':
+        local_hash = sha512sum(file_path)
+    elif hash_method == 'sha256':
+        local_hash = sha256sum(file_path)
+    elif hash_method == 'sha1':
+        local_hash = sha1sum(file_path)
+    else:
+        local_hash = sha512sum(file_path)
+
+    return hashes_match(local_hash, target_hash)
+
+
+def __hash_byte_str_iter(bytes_iter, hasher, as_hex_str=False):
+    """
+    :param bytes_iter:
+    :param hasher:
+    :param as_hex_str:
+    :return: 
+    """
+    for block in bytes_iter:
+        hasher.update(block)
+    return hasher.hexdigest() if as_hex_str else hasher.digest()
+
+
+def __file_as_block_iter(f, block_size=65536):
+    """
+    :param f:
+    :param block_size:
+    :return: 
+    """
+    with f:
+        block = f.read(block_size)
+        while len(block) > 0:
+            yield block
+            block = f.read(block_size)
--- MelonSmasher/Reflect/__init__.py
+++ MelonSmasher/Reflect/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
 
-from lxml import objectify
+from MelonSmasher.Helpers.util import *
+from requests import get
 from time import sleep
-from requests import put, get
 from subprocess import call
 import os.path
 import sys
-import hashlib
-import base64
 
 NAME_SCHEME_META = '{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}'
 NAME_SCHEME_DATA = '{http://schemas.microsoft.com/ado/2007/08/dataservices}'
@@ -56,121 +54,7 @@ class Mirror(object):
         self.verify_uploaded = verify_uploaded
         self.verify_cache = verify_cache
 
-    @staticmethod
-    def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
-        for block in bytesiter:
-            hasher.update(block)
-        return hasher.hexdigest() if ashexstr else hasher.digest()
-
-    @staticmethod
-    def file_as_blockiter(file, blocksize=65536):
-        with file:
-            block = file.read(blocksize)
-            while len(block) > 0:
-                yield block
-                block = file.read(blocksize)
-
-    def sha512sum(self, file_path, blocksize=65536):
-        """
-        :param file_path: 
-        :param blocksize: 
-        :return: 
-        """
-        return base64.encodestring(
-            self.hash_bytestr_iter(self.file_as_blockiter(open(file_path, 'rb')), hashlib.sha512(), False)).replace(
-            "\n", '')
-
-    def sha256sum(self, file_path, blocksize=65536):
-        """
-        :param file_path: 
-        :param blocksize: 
-        :return: 
-        """
-        return base64.encodestring(
-            self.hash_bytestr_iter(self.file_as_blockiter(open(file_path, 'rb')), hashlib.sha256(), False)).replace(
-            "\n", '')
-
-    @staticmethod
-    def hashes_match(hash_1, hash_2):
-        """
-        :param hash_1: 
-        :param hash_2: 
-        :return: 
-        """
-        if str(hash_1) == str(hash_2):
-            print(' Pass!')
-            return True
-        else:
-            print(' Fail!')
-            print('Hash 1: ' + hash_1)
-            print('Hash 2: ' + hash_2)
-            return False
-
-    def verify_package_hash(self, file_path, target_hash, message='Verifying package hash...'):
-        """
-        :param file_path: 
-        :param target_hash: 
-        :param message: 
-        :return: 
-        """
-        sys.stdout.write(message)
-        sys.stdout.flush()
-        local_hash = self.sha512sum(file_path)
-        return self.hashes_match(local_hash, target_hash)
-
-    @staticmethod
-    def _get(url):
-        """
-        :param url: str
-        :return: objectify.ObjectifiedElement | bool
-        """
-        response = get(url)
-        if response.status_code == 200:
-            xml = response.content
-            try:
-                response.objectified = objectify.fromstring(xml)
-                return response
-            except Exception as e:
-                print(e.message)
-                return False
-        else:
-            return response
-
-    def local_package(self, title, version):
-        """
-        :param title: 
-        :param version: 
-        :return: 
-        """
-        url = ''.join([self.local_packages_url, '(Id=\'', title, '\',Version=\'', version, '\')'])
-        return self._get(url)
-
-    def remote_package(self, title, version):
-        """
-        :param title: 
-        :param version: 
-        :return: 
-        """
-        url = ''.join([self.remote_packages_url, '(Id=\'', title, '\',Version=\'', version, '\')'])
-        return self._get(url)
-
-    def local_packages(self, url=None):
-        """
-        :param url: 
-        :return: 
-        """
-        if url is None: url = self.local_packages_url
-        return self._get(url)
-
-    def remote_packages(self, url=None):
-        """
-        :param url: 
-        :return: 
-        """
-        if url is None: url = self.remote_packages_url
-        return self._get(url)
-
-    def _download_package(self, content_url, local_path, remote_hash=None, reties=0, force=False):
+    def __download_package(self, content_url, local_path, remote_hash=None, reties=0, force=False):
         """
         :param content_url: 
         :param local_path: 
@@ -201,12 +85,12 @@ class Mirror(object):
 
         if os.path.isfile(local_path):
             if not remote_hash is None:
-                hash_verified = self.verify_package_hash(local_path, remote_hash)
+                hash_verified = verify_hash(local_path, remote_hash)
                 if not hash_verified and reties < 3:
                     # Retry loop
                     reties += 1
                     print('Hashes do not match retrying download...')
-                    return self._download_package(content_url, local_path, remote_hash, reties, True)
+                    return self.__download_package(content_url, local_path, remote_hash, reties, True)
                 elif not hash_verified and reties >= 3:
                     # Reached max retries
                     print('Retried to download the package 3 times, moving on... :-(')
@@ -221,7 +105,7 @@ class Mirror(object):
             # File did not exists after DL
             return False
 
-    def _upload_package(self, local_path, title, version, force=False):
+    def __upload_package(self, local_path, title, version, force=False):
         """
         :param local_path: 
         :param title: 
@@ -229,7 +113,7 @@ class Mirror(object):
         :param force: 
         :return: 
         """
-        local_response = self.local_package(title, version)
+        local_response = pull_package(title, version, self.local_packages_url)
         if local_response.status_code == 404 or force:
             print('Uploading package...')
             cmd = ' '.join([self.dotnet_path, 'nuget', 'push', local_path, '-s', self.local_api_upload_url, '-k',
@@ -257,7 +141,7 @@ class Mirror(object):
             }
 
         if not 'server_hash' in upload_status and self.verify_uploaded:
-            r = self.local_package(title, version)
+            r = pull_package(title, version, self.local_packages_url)
             if r.status_code == 200:
                 upload_status['server_hash'] = r.objectified[KEY_PROPERTIES][KEY_HASH]
             else:
@@ -268,6 +152,7 @@ class Mirror(object):
     def sync_and_verify_package(self, package, retry=0):
         """
         :param package: 
+        :param retry: 
         :return: 
         """
         # Extract the info that we need from the entry
@@ -285,25 +170,25 @@ class Mirror(object):
         print(''.join(['########## ', package_name, ' ##########']))
 
         if not os.path.isfile(local_path):
-            dl_status = self._download_package(content_url, local_path, remote_hash)
+            dl_status = self.__download_package(content_url, local_path, remote_hash)
         else:
             if self.verify_cache:
-                if not self.verify_package_hash(local_path, remote_hash):
+                if not verify_hash(local_path, remote_hash):
                     os.remove(local_path)
-                    dl_status = self._download_package(content_url, local_path, remote_hash, True)
+                    dl_status = self.__download_package(content_url, local_path, remote_hash, True)
                 else:
                     dl_status = True
 
         if dl_status:
-            up_status = self._upload_package(local_path, title, version)
+            up_status = self.__upload_package(local_path, title, version)
             if up_status['mirrored']:
                 if self.verify_uploaded and up_status['server_hash']:
                     sys.stdout.write('Verifying server hashes...')
                     sys.stdout.flush()
-                    if not self.hashes_match(remote_hash, up_status['server_hash']):
+                    if not hashes_match(remote_hash, up_status['server_hash']):
                         print('Mirror and repo hashes do not match! Re-uploading...')
-                        up_status = self._upload_package(local_path, title, version, True)
-                        if not self.hashes_match(remote_hash, up_status['server_hash']):
+                        up_status = self.__upload_package(local_path, title, version, True)
+                        if not hashes_match(remote_hash, up_status['server_hash']):
                             if retry < 3:
                                 retry += 1
                                 up_status = self.sync_and_verify_package(package, retry)
@@ -337,7 +222,7 @@ class Mirror(object):
         url = self.remote_packages_url
         while not done:
             # print(''.join(['Requesting packages from: ', url]))
-            response = self.remote_packages(url)
+            response = pull_packages(url)
             if response.status_code == 200:
                 page = response.objectified
                 # print(''.join(['Remote API returned: ', str(len(page.entry)), ' packages!']))
